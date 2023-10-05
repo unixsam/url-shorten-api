e67cf7e9caf9b2f9c2d91996f20ad2cd73e059ac
Sami Ahmed <unixsam@gmail.com>
2023-10-05 13:54:25 +0400

    add knex and create migration tasks

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+require('dotenv').config(); // Load environment variables from .env file
+
+module.exports = {
+    development: {
+        client: 'mysql2',
+        connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
+        },
+        migrations: {
+            directory: './db/migrations',
+        },
+        seeds: {
+            directory: './db/seeds'
+        }
+    }
+};
